1b551e910c621e500d19af497d4a6836b512a8ff
Grace Choi <oiograceoio@gmail.com>
2019-04-05 23:00:17 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -34,6 +34,14 @@ class FiguresController < ApplicationController
 
   patch '/figures/:id' do
     @figure = Figure.find_by_id(params[:id])
-
+    @figure.update(params[:figure])
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
   end
+  
 end
